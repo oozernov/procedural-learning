86e9c7c7545adea79a752d4cb037210ff94f5995
Ola Ozernov-Palchik <oozernov@mit.edu>
2020-07-24 16:01:07 -0400

    Update NewProceduralCode_07_20_20.R

--- NewProceduralCode_07_20_20.R
+++ NewProceduralCode_07_20_20.R
@@ -9,7 +9,7 @@
 #import packages 
 Packages <- c("dplyr", "stats", "psych", "ggplot2", "lme4", 
               "gridExtra", "dplyr","caret","tidyverse", "ggplot2",
-              "plyr","lmerTest","ggpubr","nlme"
+              "plyr","lmerTest","ggpubr","nlme","emmeans"
 )
 
 lapply(Packages, library, character.only = TRUE)
@@ -70,31 +70,40 @@ for (i in 1:nrow(PL)) {
   }
 }
 
+rp$On <- as.numeric(rp$On) 
+rp$Off <- as.numeric(rp$Off)
+rp$trial <- as.numeric(rp$trial)
 
 rp2<-rp%>%dplyr::group_by(PartID,Subgroup,trial)%>%
                          mutate(prop_on=On/(On+Off))
 
-rp2$Subgroup<-as.factor(rp2$Subgroup)
-rp2$trial<-as.factor(rp2$trial)
+
+### Outliers ###
+# 
+# boxplot(rp2$prop_on)$out
+# outliers <- boxplot(rp2$prop_on, plot=FALSE)$out
+# rp2<-rp2
+# rp2<- rp2[-which(rp2$prop_on %in% outliers),] #excluded 8 outliers
 
 ###Analyze ###
+rp2$Subgroup<-as.factor(rp2$Subgroup)
+rp2$trial<-as.factor(rp2$trial)
 m1<-lme(prop_on~Subgroup*trial,random=~1|PartID,
-        data=rp2)
+        data=rp2,na.action = 'na.omit')
 anova(m1)
 
 lsmeans(m1, list(pairwise ~ Subgroup|trial), adjust = "tukey") #run only for gith
 
 
 ### Plot ###
-rp$On <- as.numeric(rp$On) 
-rp$Off <- as.numeric(rp$Off)
-rp$trial <- as.numeric(rp$trial)
-rpPlot<-dplyr::summarise(group_by(rp,Subgroup,trial), n = n(),
-                         prop_on=mean(On/(On+Off)), sd=sd(On/(On+Off)),se = sd/sqrt(n))
+rp2$trial<-as.integer(rp2$trial)
 
-ggplot(data = rpPlot, aes(x=trial, y=prop_on, color = Subgroup))+
+rpPlot<-dplyr::summarise(group_by(rp2,Subgroup,trial), n = n(),
+                         mean=mean(prop_on), sd=sd(On/(On+Off)),se = sd/sqrt(n))
+
+ggplot(data = rpPlot, aes(x=trial, y=mean, color = Subgroup))+
   geom_line() +geom_point()  +
-  geom_errorbar(aes(ymin=prop_on-se,ymax=prop_on+se),
+  geom_errorbar(aes(ymin=mean-se,ymax=mean+se),
                  width=.1,  size=0.5)+
   scale_x_continuous(breaks=seq(2,16,1))+
   #scale_y_continuous(limits = c(0.2, 0.5))+
@@ -112,7 +121,7 @@ ggplot(data = rpPlot, aes(x=trial, y=prop_on, color = Subgroup))+
 #On Slopes 
 d_glm_fit_list <- vector(mode = "list", length = nrow(rp)*2)
 index <- 0
-abcd_ids=unique(rp$PartID)
+abcd_ids=unique(rp2$PartID)
 b_list=c()
 m_list = c()
 
@@ -120,7 +129,7 @@ for (subj in abcd_ids) { #d_export is my list of subject
   #for (a in c("d", "b")) 
   # index <- index + 1
   print(subj)
-  d_subj <- filter(rp, PartID==subj)
+  d_subj <- filter(rp2, PartID==subj)
   subj_model <- glm(On ~ trial, #complettion time(On/Off) ~trial
                     data = d_subj)
   b_list[subj] <- as.numeric(subj_model$coefficients[1]) # coefficient intercept
@@ -140,7 +149,7 @@ for (subj in abcd_ids) { #d_export is my list of subject
   #for (a in c("d", "b")) 
   # index <- index + 1
   print(subj)
-  d_subj <- filter(rp, PartID==subj)
+  d_subj <- filter(rp2, PartID==subj)
   subj_model <- glm(Off ~ trial, 
                     data = d_subj)
   b_list2[subj] <- as.numeric(subj_model$coefficients[1]) # coefficient intercept
@@ -149,7 +158,6 @@ for (subj in abcd_ids) { #d_export is my list of subject
 }
 
 #PropOn Slopes
-rp$Prop_On <- as.numeric(rp$On)/as.numeric(rp$Off+rp$On)
 
 d_glm_fit_list <- vector(mode = "list", length = nrow(rp)*2)
 index <- 0
@@ -162,8 +170,8 @@ for (subj in abcd_ids) { #d_export is my list of subject
   #for (a in c("d", "b")) 
   # index <- index + 1
   print(subj)
-  d_subj <- filter(rp, PartID==subj)
-  subj_model <- glm(Prop_On ~ trial, #complettion time(On/Off) ~trial
+  d_subj <- filter(rp2, PartID==subj)
+  subj_model <- glm(prop_on ~ trial, #complettion time(On/Off) ~trial
                     data = d_subj)
   b_list4[subj] <- as.numeric(subj_model$coefficients[1]) # coefficient intercept
   m_list4[subj] <- as.numeric(subj_model$coefficients[2]) # coefficient slope
@@ -187,7 +195,9 @@ plot_rp_slope<-d2%>%dplyr::select("Subgroup","slopeProp_On_t")
 ggdensity(plot_rp_slope, x = "slopeProp_On_t",
           add = "mean", rug = TRUE,
           color = "Subgroup", fill = "Subgroup",
-          palette = c("#00AFBB", "#E7B800"))
+          #palette = c("#00AFBB", "#E7B800"))
+          )
+          
 
 
 ####Mirror Tracing####
@@ -218,12 +228,21 @@ mt$time <- as.numeric(mt$time)
 mt$error <- as.numeric(mt$error)
 mt$trial <- as.factor(mt$trial)
 
-mt2<-na.omit(mt)
+### Outliers ###
+
+#boxplot(mt$error)$out
+#boxplot(mt$time)$out
+#dev.off()
+#outliers <- boxplot(mt$error, plot=FALSE)$out
+#mt3<-mt2
+#mt2<- mt2[-which(mt2$error %in% outliers),] #excluded 8 outliers
 
 ###Analyze ###
+
+mt2<-na.omit(mt)
 m3<-nlme::lme(time~Subgroup*trial,random=~1|PartID,
         data=mt2)
-anova(m3)
+anova(m3) #trial is only one sig
 
 m4<-nlme::lme(error~Subgroup*trial,random=~1|PartID,
               data=mt2)
@@ -231,88 +250,94 @@ anova(m4)
 
 
 ### Plot ###
-mt2$time <- as.numeric(mt2$time) 
-mt2$error <- as.numeric(mt2$error)
-mt2$trial <- as.numeric(mt2$trial)
-
-mtTPlot<-dplyr::summarise(group_by(mt2,Subgroup,trial), n = n(),
-                         meanT=mean(time), sd=sd(time),se = sd/sqrt(n))
-mtEPlot<-dplyr::summarise(group_by(mt2,Subgroup,trial), n = n(),
-                          meanE=mean(error), sd=sd(error),se = sd/sqrt(n))
-
-
-ggplot(data = mtTPlot, aes(x=trial, y=meanT, color = Subgroup))+
-  geom_line() +geom_point()  +
-  geom_errorbar(aes(ymin=meanT-se,ymax=meanT+se),
-                width=.1,  size=0.5)+
-  scale_x_continuous(breaks=seq(0,10,1))+
-  #scale_y_continuous(limits = c(0.2, 0.5))+
-  theme(
-    axis.title = element_text(family = "Trebuchet MS", size = 20),
-    legend.key.size = unit(1, "cm"),
-    axis.text.x = element_text(size = 15),
-    axis.text.y = element_text(size = 15))  + 
-  labs(x = "Trials", y = "Completion Time (secs)") +
-  theme(panel.background = element_rect(fill = "white", colour = "grey50")) + 
-  theme(axis.line = element_line(arrow = arrow(angle = 15, length = unit(.15,"inches"),type = "closed")))
-
-ggplot(data = mtEPlot, aes(x=trial, y=meanE, color = Subgroup))+
-  geom_line() +geom_point()  +
-  geom_errorbar(aes(ymin=meanE-se,ymax=meanE+se),
-                width=.1,  size=0.5)+
-  scale_x_continuous(breaks=seq(0,10,1))+
-  #scale_y_continuous(limits = c(0.2, 0.5))+
-  theme(
-    axis.title = element_text(family = "Trebuchet MS", size = 20),
-    legend.key.size = unit(1, "cm"),
-    axis.text.x = element_text(size = 15),
-    axis.text.y = element_text(size = 15))  + 
-  labs(x = "Trials", y = "Errors") +
-  theme(panel.background = element_rect(fill = "white", colour = "grey50")) + 
-  theme(axis.line = element_line(arrow = arrow(angle = 15, length = unit(.15,"inches"),type = "closed")))
-
-
+ 
 
 ### Slopes ###
 
 #slope based on error
-#d_list <- vector(mode = "list", length = nrow(mt)*2)
-#index <- 0
 
-abcd_m_ids=unique(mt$PartID)
+d_glm_fit_list <- vector(mode = "list", length = nrow(rp)*2)
+index <- 0
+abcd_ids=unique(mt$PartID)
 b_list5=c()
 m_list5 = c()
 
-for (subj in abcd_m_ids) { #d_export is my list of subject 
-  # index <- index + 1
+for (subj in abcd_ids) { #d_export is my list of subject 
   print(subj)
-  d_m_subj <- filter(mt2, PartID==subj)
-  subj_m_model <- lm(error ~ trial, #complettion time(On/Off) ~trial
-                      data = d_m_subj)
-  b_list5[subj] <- as.numeric(subj_m_model$coefficients[1]) # coefficient intercept
-  m_list5[subj] <- as.numeric(subj_m_model$coefficients[2]) # coefficient slope
+  d_subj <- dplyr::filter(mt, PartID==subj)
+  #subj_model <- glm(error ~ trial, #errors
+                    #data = d_subj)
+  subj_model<- glm(error ~ ns(trial,3), data=d_subj,family = "poisson")
+  b_list5[subj] <- as.numeric(subj_model$coefficients[1]) # coefficient intercept
+  m_list5[subj] <- as.numeric(subj_model$coefficients[2]) # coefficient slope
 }
 
-#slope based on time
-#d_list2 <- vector(mode = "list", length = nrow(mt)*2)
-#index <- 0
+
 b_list7=c()
 m_list7 = c()
 
-for (subj in abcd_m_ids) { #d_export is my list of subject 
+
+d_glm_fit_list <- vector(mode = "list", length = nrow(rp)*2)
+index <- 0
+abcd_ids=unique(mt$PartID)
+b_list7=c()
+m_list7 = c()
+mt$trial<-as.numeric(mt$trial)
+for (subj in abcd_ids) { #d_export is my list of subject 
   print(subj)
-  d_m_subj <- filter(mt2, PartID==subj)
-  subj_m_model <- glm(time ~ trial, #complettion time~trial
-                      data = d_m_subj)
-  b_list7[subj] <- as.numeric(subj_m_model$coefficients[1]) # coefficient intercept
-  m_list7[subj] <- as.numeric(subj_m_model$coefficients[2]) # coefficient slope
+  d_subj <- dplyr::filter(mt, PartID==subj)
+  #subj_model <- glm(time ~ trial, #complettion time(On/Off) ~trial
+                    #data = d_subj,na.action = 'na.omit')
+  subj_model<- glm(time ~ ns(trial,3), data=d_subj,family = "poisson")
+  b_list7[subj] <- as.numeric(subj_model$coefficients[1]) # coefficient intercept
+  m_list7[subj] <- as.numeric(subj_model$coefficients[2]) # coefficient slope
   
 }
 
 #new data frame containing the mirror slopes
-slope_m_data <- data.frame(PartID=abcd_m_ids,slope_me = m_list5, slope_mt = m_list7)
+mt_data2 <- cbind(PartID=abcd_ids,slope_me = m_list5, slope_mt = m_list7)
+mt_data<-as.data.frame(mt_data)
+mt_data$slope_me_t<-abs(as.numeric(mt_data$slope_me))
+mt_data$slope_me_t<-log10(mt_data$slope_me_t+1) #transform slope
+
+mt_data$slope_mt_t<-abs(as.numeric(mt_data$slope_mt))
+mt_data$slope_mt_t<-log10(mt_data$slope_mt_t+1) #transform slope
+
+d2<-merge(d2, mt_data)
+
+#Analyze
+m3<-lm(slope_mt_t~background_age+background_sex+kbit_ss+Subgroup, data=d2)
+anova(m3)
+m4<-lm(slope_me_t~background_age+background_sex+kbit_ss+Subgroup, data=d2)
+anova(m4) #sig group difference
+
+###plot slope ###
+
+plot_me_slope<-d2%>%dplyr::select("Subgroup","slope_me_t")
+
+ggpubr::ggdensity(plot_me_slope, x = "slope_me_t",
+          add = "mean", rug = TRUE,
+          color = "Subgroup", fill = "Subgroup",
+          #palette = c("#00AFBB", "#E7B800"))
+)
+
+plot_mt_slope<-d2%>%dplyr::select("Subgroup","slope_mt_t")
+ggdensity(plot_mt_slope, x = "slope_mt_t",
+          add = "mean", rug = TRUE,
+          color = "Subgroup", fill = "Subgroup",
+          #palette = c("#00AFBB", "#E7B800"))
+)
 
 
+
+#plot slopes
+mt_slope<-d2%>%dplyr::select("Subgroup","slopeProp_On_t")
+
+ggdensity(plot_rp_slope, x = "slopeProp_On_t",
+          add = "mean", rug = TRUE,
+          color = "Subgroup", fill = "Subgroup",
+          #palette = c("#00AFBB", "#E7B800"))
+)
 #### Statistical Learning ####
 names(SL)[names(SL) == "PartID"] <- "ABCD.ID"
 d2<-merge(d,SL)
