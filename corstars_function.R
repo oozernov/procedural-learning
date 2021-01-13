725c7eedeeba46df6386ae851b640363c377b450
Zhenghan Qi <zhenghan.qi@gmail.com>
2021-01-12 20:32:01 -0500

    change the p-threshold in corstars function to allow for one-tailed correlation; added notes about lmer and growth-curve analysis

--- corstars_function.R
+++ corstars_function.R
@@ -13,8 +13,8 @@ corstars <-function(x, method=c("pearson", "spearman"), removeTriangle=c("upper"
   R <- correlation_matrix$r # Matrix of correlation coeficients
   p <- correlation_matrix$P # Matrix of p-value 
   
-  ## Define notions for significance levels; spacing is important.
-  mystars <- ifelse(p < .0001, "****", ifelse(p < .001, "*** ", ifelse(p < .01, "**  ", ifelse(p < .05, "*   ", "    "))))
+  ## Define notions for significance levels; spacing is important.(ZQ notes: these stars are for one-tailed significance testing)
+  mystars <- ifelse(p < .0002, "****", ifelse(p < .002, "*** ", ifelse(p < .02, "**  ", ifelse(p < .1, "*   ", "    "))))
   
   ## trunctuate the correlation matrix to two decimal
   R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1]
